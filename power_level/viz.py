dfc2882cd7ec8b06eb1d8ce85ac55ab45a9b6ac3
JerryHan88 <jerryhan88@gmail.com>
2017-02-14 22:52:28 +0800

    t

--- power_level/viz.py
+++ power_level/viz.py
@@ -1,12 +1,13 @@
 from bisect import bisect
 import wx
-
+import wx.lib.buttons
 
 MARGIN = 10
 WHITE = wx.Colour(255, 255, 255)
 ORANGE = wx.Colour(228, 108, 10)
 RED = wx.Colour(255, 0, 0)
 BLACK = wx.Colour(0, 0, 0)
+GRAY = wx.Colour(220, 220, 220)
 #
 
 purple = wx.Colour(115, 28, 96)
@@ -18,41 +19,68 @@ light_orange = wx.Colour(248, 238, 211)
 dark_orange = wx.Colour(238, 108, 0)
 
 class beaconViewer(wx.Frame):
-    def __init__(self, title, numRows, numCols, pos, size, style=wx.DEFAULT_FRAME_STYLE):
-        wx.Frame.__init__(self, None, -1, title, pos, size, style)
+    def __init__(self, title, numRows, numCols, pos, size):
+        no_resize = wx.DEFAULT_FRAME_STYLE & ~ (wx.RESIZE_BORDER |
+                                                wx.RESIZE_BOX |
+                                                wx.MAXIMIZE_BOX)
+        wx.Frame.__init__(self, None, -1, title, pos, size, no_resize)
         self.basePanel = wx.Panel(self, -1, pos=(0, 0), size=size)
         self.basePanel.SetBackgroundColour(WHITE)
-
         px, py = self.basePanel.GetPosition()
         sx, sy = self.basePanel.GetSize()
-
+        #
         horizonRatio = (1, 1)
         horizonUnit = (sx - (len(horizonRatio) + 1) * MARGIN) / sum(horizonRatio)
         verticalRatio = (4, 1)
         verticalUnit = (sy - (len(verticalRatio) + 1) * MARGIN) / sum(verticalRatio)
-        self.initGrid((MARGIN +px , MARGIN + py), (horizonUnit * horizonRatio[0], verticalUnit * verticalRatio[0]))
+        self.gridPanel = self.initGrid((MARGIN + px, MARGIN + py),
+                                       (horizonUnit * horizonRatio[0], verticalUnit * verticalRatio[0]))
         sx, sy = self.gridPanel.GetSize()
-
-        chartPanel = self.initChart((px + MARGIN + sx + MARGIN, py + MARGIN), (horizonUnit * horizonRatio[1], verticalUnit * sum(verticalRatio) - MARGIN))
-        chartPanel.SetBackgroundColour(RED)
-
-
-        colUnit= sx / float(numCols)
+        colUnit = sx / float(numCols)
         rowUnit = sy / float(numRows)
         self.xPoints, self.yPoints = [i * colUnit for i in range(numCols)], [j * rowUnit for j in range(numRows)]
-        print self.xPoints, self.yPoints
+        #
+        sx, sy = self.gridPanel.GetSize()
+        self.initControls((MARGIN + px, MARGIN + py + MARGIN + sy),
+                          (horizonUnit * horizonRatio[0], verticalUnit * verticalRatio[1] - 2 * MARGIN))
+        #
+        sx, sy = self.gridPanel.GetSize()
+        chartPanel = self.initChart((MARGIN + px + MARGIN + sx, MARGIN + py),
+                                    (horizonUnit * horizonRatio[1], verticalUnit * sum(verticalRatio) - MARGIN))
+        chartPanel.SetBackgroundColour(RED)
+        #
         self.zones = {}
         for i in range(numCols):
             for j in range(numRows):
                 self.zones[i, j] = zone(i, j, colUnit, rowUnit)
+        self.Bind(wx.EVT_CLOSE, self.OnQuit)
+
+    def OnQuit(self, _):
+        self.Destroy()
+
+    def initControls(self, _pos, _size):
+        controlPanel = wx.Panel(self.basePanel, -1, pos=_pos, size=_size)
+        controlPanel.SetBackgroundColour(purple)
+        #
+        sx, sy = controlPanel.GetSize()
+        bSx, bSy = sx / float(3), sy / float(2)
+        qx, cy = sx / float(4), sy / float(2)
+
+        solBtn = wx.lib.buttons.GenButton(controlPanel, label="solve", pos=(qx - bSx / 2, cy - bSy / 2), size=(bSx, bSy))
+        solBtn.SetFont(wx.Font(bSy / 2, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+        solBtn.SetBackgroundColour(GRAY)
+        controlPanel.Bind(wx.EVT_BUTTON, self.runHeuristic, solBtn)
+
+    def runHeuristic(self, _):
+        print 'run'
 
     def initGrid(self, _pos, _size):
-        self.gridPanel = wx.Panel(self.basePanel, -1, pos=_pos, size=_size)
-        self.gridPanel.SetBackgroundColour(purple)
+        gridPanel = wx.Panel(self.basePanel, -1, pos=_pos, size=_size)
         #
-        self.gridPanel.Bind(wx.EVT_PAINT, self.OnPaint)
-        self.gridPanel.Bind(wx.EVT_LEFT_DOWN, self.OnLeftClick)
-        self.gridPanel.Bind(wx.EVT_RIGHT_DOWN, self.OnRightClick)
+        gridPanel.Bind(wx.EVT_PAINT, self.OnPaint)
+        gridPanel.Bind(wx.EVT_LEFT_DOWN, self.OnLeftClick)
+        gridPanel.Bind(wx.EVT_RIGHT_DOWN, self.OnRightClick)
+        return gridPanel
 
 
     def initChart(self, _pos, _size):
@@ -114,7 +142,7 @@ class zone(object):
 
 if __name__ == '__main__':
     app = wx.App(False)
-    numRows, numCols = 5, 6
+    numRows, numCols = 10, 12
     mv = beaconViewer('Viewer', numRows, numCols, pos=(200, 200), size=(1200, 600))
     mv.Show(True)
     app.MainLoop()
